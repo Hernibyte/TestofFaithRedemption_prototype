bdc992e62fc786190b889c84b26272022488f540
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-09-19 11:02:27 -0300

    first preview of menu scene

---
+++ Assets/Prototype1/Scritps/MainMenu/MenuSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSystem : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Prototype1");
+    }
+
+    public void Options()
+    {
+        Debug.Log("Null");
+    }
+
+    public void Credits()
+    {
+        Debug.Log("Null");
+    }
+}
