5c06861f3d9e68936c31bd3f316a23c480a3ffd6
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-09-20 14:30:57 -0300

    Added a sample of the doors tp

---
+++ Assets/Prototype1/Scritps/Game/LevelGenerator/NewSystem/GateBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateBehaviour : MonoBehaviour
+{
+    enum Side
+    {
+        Left,
+        Right,
+        Top,
+        Down
+    }
+    [SerializeField] Side side;
+    [SerializeField] LayerMask playerMask;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (Contains(playerMask, collision.gameObject.layer))
+        {
+            switch (side)
+            {
+                case Side.Right:
+                    collision.gameObject.transform.position = RePosition(2f, 0f, 0f);
+                    break;
+                case Side.Left:
+                    collision.gameObject.transform.position = RePosition(-2f, 0f, 0f);
+                    break;
+                case Side.Down:
+                    collision.gameObject.transform.position = RePosition(0f, -2f, 0f);
+                    break;
+                case Side.Top:
+                    collision.gameObject.transform.position = RePosition(0f, 2f, 0f);
+                    break;
+            }
+        }
+    }
+
+    Vector3 RePosition(float x, float y, float z)
+    {
+        Vector3 newPos = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z + z);
+        return newPos;
+    }
+
+    bool Contains(LayerMask mask, int layer)
+    {
+        return mask == (mask | (1 << layer));
+    }
+}
