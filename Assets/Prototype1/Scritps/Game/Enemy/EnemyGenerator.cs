ed0bd6a91eb78620b920708548261de1cacc36d3
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-09-26 17:31:01 -0300

    Improved loot spawn system and improved enemy spawn

--- Assets/Prototype1/Scritps/Game/Enemy/EnemyGenerator.cs
+++ Assets/Prototype1/Scritps/Game/Enemy/EnemyGenerator.cs
@@ -9,28 +9,40 @@ public class EnemyGenerator : MonoBehaviour
     [SerializeField] GameObject spawnArea;
     [SerializeField] bool enemyCreated;
     [SerializeField] GameObject lootChest;
-
-    [SerializeField] public int rangersInRoom;
+    [Header("Enemies Amount")]
+    [SerializeField] int ragnersEnemyAmount;
+    [SerializeField] int meleeEnemyAmount;
+    [Header("only 0 or 1")]
+    [SerializeField] int rangersMinAmount;
+    [SerializeField] int meleeMinAmount;
+    int rangersInRoom;
+    int meleeEnemiesInRoom;
+    [Header("")]
 
     [SerializeField] public int enemiesAlive;
 
     [SerializeField] List<GameObject> spawnPoints;
 
-    [SerializeField] MeleeEnemy warriorEnemy; //Lpm con no poder crear mas de uno hay que cambiar el loot system xd
-    [SerializeField] RangeEnemy[] rangedEnemies;
+    //[SerializeField] MeleeEnemy warriorEnemy; //Lpm con no poder crear mas de uno hay que cambiar el loot system xd | igual es innecesario pa
+    //[SerializeField] RangeEnemy[] rangedEnemies; // Esto que hiciste es mas rancio que todo mi codigo junto facu lpm
 
     public bool imSpawnBoss;
     public UI_Boss uiBoss;
 
     private void Start()
     {
+
         enemyCreated = false;
-        rangedEnemies = new RangeEnemy[rangersInRoom];
+        //rangedEnemies = new RangeEnemy[rangersInRoom]; // Porque!!
     }
 
     void DecreaseEnemiesAliveRoom()
     {
         enemiesAlive--;
+        if(enemiesAlive <= 0)
+        {
+            SpawnLoot();
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -43,27 +55,42 @@ public class EnemyGenerator : MonoBehaviour
         }
         if(!enemyCreated)
         {
-            int randomSpawner = Random.Range(0, spawnPoints.Count-1);
-            MeleeEnemy obj = Instantiate(prefabEnemyWarrior, spawnPoints[randomSpawner].transform.position, Quaternion.identity);
-            warriorEnemy = obj;
-            warriorEnemy.deathEvent.AddListener(DecreaseEnemiesAliveRoom);
-
-            enemiesAlive++;
+            meleeEnemiesInRoom = Random.Range(meleeMinAmount, meleeEnemyAmount+1);
+            rangersInRoom = Random.Range(rangersMinAmount, ragnersEnemyAmount+1);
+            //
+            int randomSpawner = 0;
+            for(int i = 0; i < meleeEnemiesInRoom; i++)
+            {
+                randomSpawner = Random.Range(0, spawnPoints.Count);
+                MeleeEnemy obj = Instantiate(prefabEnemyWarrior, spawnPoints[randomSpawner].transform.position, Quaternion.identity);
+                obj.deathEvent.AddListener(DecreaseEnemiesAliveRoom);
+                enemiesAlive++;
+            }
             for (int i = 0; i < rangersInRoom; i++)
             {
-                randomSpawner = Random.Range(0, spawnPoints.Count-1);
-                rangedEnemies[i] = Instantiate(prefabEnemyRanger, spawnPoints[randomSpawner].transform.position, Quaternion.identity);
-                rangedEnemies[i].deathEvent.AddListener(DecreaseEnemiesAliveRoom);
+                randomSpawner = Random.Range(0, spawnPoints.Count);
+                //rangedEnemies[i] = Instantiate(prefabEnemyRanger, spawnPoints[randomSpawner].transform.position, Quaternion.identity); // Que es esto!!
+                //rangedEnemies[i].deathEvent.AddListener(DecreaseEnemiesAliveRoom); Como vas a poner esto asi forro jajajaj
+                RangeEnemy obj = Instantiate(prefabEnemyRanger, spawnPoints[randomSpawner].transform.position, Quaternion.identity);
+                obj.deathEvent.AddListener(DecreaseEnemiesAliveRoom);
                 enemiesAlive++;
             }
             enemyCreated = true;
-            obj.deathEvent.AddListener(SpawnLoot);
         }
     }
 
     void SpawnLoot()
     {
-        Instantiate(lootChest, spawnArea.transform.position, Quaternion.identity);
+        int rand = Random.Range(0, 2);
+        switch(rand)
+        {
+            case 1:
+                Instantiate(lootChest, spawnArea.transform.position, Quaternion.identity);
+                break;
+            default:
+                //nada jaja salu2
+                break;
+        }
     }
 
     public void FindUI_Boss()
